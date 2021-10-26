e4795881d6078e54d6c5a8aebc5c6750a098d783
Achmad Sufyan Aziz <49118546+sufyanaziz@users.noreply.github.com>
2021-10-26 17:42:15 +0700

    add sticky and scroll css

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,16 +1,76 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
 
-const Navbar = () => {
+const Navbar: React.FC<{ id: string }> = ({ id }) => {
+  const [isSticky, setIsSticky] = useState<boolean>(false);
+
+  const handleScroll = () => {
+    const elementNavbar = document.querySelector("#navbar-section");
+    const elementNavbarOffset = elementNavbar?.getBoundingClientRect().top;
+
+    if (elementNavbarOffset === 0) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => handleScroll());
+
+    return window.removeEventListener("scroll", () => handleScroll());
+  }, []);
+
   return (
-    <Box sx={{ px: 0, py: 3, display: "flex", color: "var(--mainColor)" }}>
-      <Typography variant="subtitle1" component="nav">
+    <Box
+      id={id}
+      sx={{
+        px: isSticky === true ? 3 : 0,
+        py: 3,
+        display: "flex",
+        position: "sticky",
+        top: 0,
+        color: isSticky === true ? "white" : "var(--mainColor)",
+        zIndex: 2,
+        background: isSticky === true ? "#056490" : "white",
+      }}
+    >
+      <Typography
+        variant="subtitle1"
+        component="nav"
+        onClick={() => {
+          const anchor = document.querySelector("#header-section");
+          if (anchor === null) return;
+          anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+        }}
+        style={{ cursor: "pointer" }}
+      >
         Home
       </Typography>
-      <Typography variant="subtitle1" component="nav" sx={{ px: 4 }}>
+      <Typography
+        variant="subtitle1"
+        component="nav"
+        sx={{ px: 4 }}
+        onClick={() => {
+          const anchor = document.querySelector("#skills-section");
+          if (anchor === null) return;
+          anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+        }}
+        style={{ cursor: "pointer" }}
+      >
         Skill
       </Typography>
-      <Typography variant="subtitle1" component="nav">
+      <Typography
+        variant="subtitle1"
+        component="nav"
+        onClick={() => {
+          const anchor = document.querySelector("#projects-section");
+          if (anchor === null) return;
+          anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+        }}
+        style={{ cursor: "pointer" }}
+      >
         Project
       </Typography>
     </Box>
